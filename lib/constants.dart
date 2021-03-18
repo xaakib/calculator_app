e5668647b0a3379fe219ee448300fd20821b3647
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-18 17:13:36 +0600

    working on ui

---
+++ lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const kBlackColorsText = Colors.black;
+
+const kOrangeColorsText = Colors.deepOrange;
+
+const kOperationTextStyle = TextStyle(
+  fontSize: 30,
+  color: Colors.blue,
+  fontFamily: "MontserratLight",
+  fontWeight: FontWeight.w100,
+);
+
+const kResultTextStyle = TextStyle(
+  fontSize: 70,
+  color: Colors.orange,
+  fontFamily: "MontserratLight",
+  fontWeight: FontWeight.w100,
+);
--- lib/screens/calculator_page.dart
+++ lib/screens/calculator_page.dart
@@ -1,3 +1,5 @@
+import 'package:calculator_app/constants.dart';
+import 'package:calculator_app/screens/calculator_brain.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorPage extends StatefulWidget {
@@ -8,10 +10,53 @@ class CalculatorPage extends StatefulWidget {
 class _CalculatorPageState extends State<CalculatorPage> {
   String result = '0';
 
+  CalculatorBrain calc = CalculatorBrain();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Calculator App")),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 60),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: 300,
+                    maxHeight: 70,
+                  ),
+                  child: Text(
+                    result,
+                    style: kResultTextStyle,
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+              child: Text(
+                calc.resultOperationText,
+                style: kOperationTextStyle,
+                softWrap: false,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [],
+                ))
+          ],
+        ),
+      )),
     );
   }
 }
